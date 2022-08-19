cbb5d0731015a130d7a37f83f3a14b6b4572d142
Iryna Hordiichuk <mason.iryna2@gmail.com>
2022-08-19 20:32:15 +0100

    adds libraries

---
+++ src/fetch-pictures.js
@@ -0,0 +1,5 @@
+
+let pageNumber = 1;
+const PER_PAGE = 40;
+const KEY = '29377989-ce3619f612f3ee2439940f074';
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './css/styles.css';
-
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+const axios = require('axios');
 const DEBOUNCE_DELAY = 300;
+
+Notiflix.Report.info("Sorry, there are no images matching your search query. Please try again.");
+Notiflix.Report.warning("We're sorry, but you've reached the end of search results.");
+Notiflix.Report.success("Hooray! We found totalHits images.");
+
+const refs = {
+    form: document.querySelector('#search-form'),
+    input: document.querySelector('searchQuery'),
+    gallery: document.querySelector('gallery'),
+    loadMoreBtn: document.querySelector('load-more'),
+}
+
+refs.form.addEventListener('submit', submitHandler);
+refs.loadMoreBtn.addEventListener('click', loadMoreHandler);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionPosition: 'bottom',
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+
+  function loadMoreHandler(){}
+
+  function submitHandler(event){
+    event.preventDefault();
+    const searchQuery = event.currentTarget.elements.query.value;
+    console.log(searchQuery);
+    api.fetchPictures(searchQuery)
+    .then (data => galleryMarkUp(data));
+  }
+
+  function galleryMarkup (){
+    refs.gallery.innerHTML = galleryTemplate(data);
+
+  }
+
+  function clearGallery(){}
