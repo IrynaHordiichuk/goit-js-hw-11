132dbcf75c0fc79e171353b5fd612024e9a85da0
Iryna Hordiichuk <mason.iryna2@gmail.com>
2022-08-20 18:18:51 +0100

    Update

--- src/fetch-pictures.js
+++ src/fetch-pictures.js
@@ -1,5 +1,13 @@
 
-let pageNumber = 1;
+
+import axios from 'axios';
+
+export default async function fetchImages( page, searchQuery){
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const PER_PAGE = 40;
 const KEY = '29377989-ce3619f612f3ee2439940f074';
 
+    return await axios.get(`?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`);
+
+}
--- src/index.js
+++ src/index.js
@@ -1,46 +1,130 @@
-import './css/styles.css';
 import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-const axios = require('axios');
-const DEBOUNCE_DELAY = 300;
+import fetchImages from './fetch-pictures';
 
-Notiflix.Report.info("Sorry, there are no images matching your search query. Please try again.");
-Notiflix.Report.warning("We're sorry, but you've reached the end of search results.");
-Notiflix.Report.success("Hooray! We found totalHits images.");
+let currentPage = 1;
+let currentValue = '';
 
 const refs = {
-    form: document.querySelector('#search-form'),
-    input: document.querySelector('searchQuery'),
-    gallery: document.querySelector('gallery'),
-    loadMoreBtn: document.querySelector('load-more'),
-}
+  form: document.querySelector('#search-form'),
+  //     input: document.querySelector('searchQuery'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
+};
+
+refs.loadMoreBtn.style.display = 'none';
 
 refs.form.addEventListener('submit', submitHandler);
 refs.loadMoreBtn.addEventListener('click', loadMoreHandler);
 
-const gallery = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionSelector: 'img',
-    captionPosition: 'bottom',
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
 
+function submitHandler(event) {
+  event.preventDefault();
+  refs.loadMoreBtn.style.display = 'none';
+  currentValue = event.currentTarget.elements.searchQuery.value;
+  currentPage = 1;
+  fetchImages(currentPage, currentValue).then(data =>{
+    if(data.data.hits.length === 0){
+Notiflix.Report.info("Sorry, there are no images matching your search query. Please try again.");
+return
+    }
+    
+    galleryMarkup(data.data.hits);
+    if(data.data.totalHits > 40){ 
+      refs.loadMoreBtn.style.display = 'inline'
+    }
+});
+}
 
-  function loadMoreHandler(){}
-
-  function submitHandler(event){
-    event.preventDefault();
-    const searchQuery = event.currentTarget.elements.query.value;
-    console.log(searchQuery);
-    api.fetchPictures(searchQuery)
-    .then (data => galleryMarkUp(data));
+function loadMoreHandler() {
+  
+  fetchImages(currentPage, currentValue).then(data =>{
+    if((data.data.totalHits - currentPage * 40) <= 40){
+      refs.loadMoreBtn.style.display = 'none';
+      Notiflix.Report.warning("We're sorry, but you've reached the end of search results.");
+    }
+    currentPage += 1;
+    console.log(data.data.totalHits);
+    galleryMarkup(data.data.hits)
   }
+  );
+}
 
-  function galleryMarkup (){
-    refs.gallery.innerHTML = galleryTemplate(data);
+function galleryMarkup(array) {
+  const markup = array
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+        <div class="photo-card">
+        <img src=${webformatURL} alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>${views}</b>
+          </p>
+          <p class="info-item">
+            <b>${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>${downloads}</b>
+          </p>
+        </div>
+      </div>
+      `;
+      }
+    )
+    .join('');
 
-  }
+  console.log(refs.gallery);
+  refs.gallery.insertAdjacentHTML('afterbegin', markup);
+}
 
-  function clearGallery(){}
+// Notiflix.Report.info("Sorry, there are no images matching your search query. Please try again.");
+// Notiflix.Report.warning("We're sorry, but you've reached the end of search results.");
+// Notiflix.Report.success("Hooray! We found totalHits images.");
+
+// const refs = {
+//     form: document.querySelector('#search-form'),
+//     input: document.querySelector('searchQuery'),
+//     gallery: document.querySelector('gallery'),
+//     loadMoreBtn: document.querySelector('load-more'),
+// }
+
+// refs.form.addEventListener('submit', submitHandler);
+// refs.loadMoreBtn.addEventListener('click', loadMoreHandler);
+
+// const gallery = new SimpleLightbox('.gallery a', {
+//     captions: true,
+//     captionSelector: 'img',
+//     captionPosition: 'bottom',
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//   });
+
+//   function loadMoreHandler(){}
+
+//   function submitHandler(event){
+//     event.preventDefault();
+//     const searchQuery = event.currentTarget.elements.query.value;
+//     console.log(searchQuery);
+//     api.fetchPictures(searchQuery)
+//     .then (data => galleryMarkUp(data));
+//   }
+
+//   function galleryMarkup (){
+//     refs.gallery.innerHTML = galleryTemplate(data);
+
+//   }
+
+//   function clearGallery(){}
